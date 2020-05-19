71b36a08cd381b4467fb1391eb52d7d1c8578d4f
Ho Minh Chung <hmchung@tma.com.vn>
2020-05-19 20:23:43 +0700

    feat: implement ocr feature

--- Backend_LayoutService/LayoutService.API/Services/Implementation/TemplateService.cs
+++ Backend_LayoutService/LayoutService.API/Services/Implementation/TemplateService.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using LayoutService.API.Infrastructure;
 using LayoutService.API.Model;
+using LayoutService.API.Respotiroties;
 
 namespace LayoutTemplate.API.Services.Implementation
 {
